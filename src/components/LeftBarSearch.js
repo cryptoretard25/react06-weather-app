337a525a4c6319318a55966f089c2ce9d647138a
cryptoretard25 <mikhmikhlobanov@gmail.com>
2023-10-13 02:29:50 +0300

    search fuctionality - done, onclick onkeydown

--- src/components/LeftBarSearch.js
+++ src/components/LeftBarSearch.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { HeaderContext } from "../context/HeaderContextProvider";
 
 function LeftBarSearch() {
+  const { setCurrentLocation } = useContext(HeaderContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearchClick = (e)=>{
+    e.preventDefault()
+    setCurrentLocation(inputValue)
+    setInputValue('')
+  }
+
+  const handleKeyDown = (e)=>{
+    if(e.key === 'Enter' && inputValue){
+      e.preventDefault()
+      setCurrentLocation(inputValue)
+      setInputValue('')
+    }
+  }
+
   return (
     <div className="search-container">
       <input
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
         type="text"
         name="search"
         placeholder="Search..."
       />
-      <div className="search-btn">
+      <div className="search-btn" onClick={handleSearchClick}>
         <img src="./imgs/search-icon.png" alt="" />
       </div>
     </div>
